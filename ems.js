5a742c6191f907ccda19f8b2774bdcbea4300d33
Seunghoon Lee <seunghoonlee@seunghoons-mbp.home>
2020-12-11 08:45:27 -0500

    Main menu created

--- ems.js
+++ ems.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const { CONNREFUSED } = require('dns');
 
 const conn = mysql.createConnection({
     host: "localhost",
@@ -19,8 +20,25 @@ function ems_init() {
     conn.query("SELECT * FROM employee", function(err, res) {
         if (err) throw err;
         console.log(res);
+        main(res);
         // conn.end();
     })
 };
 
-
+function main(res) {
+    inquirer.prompt({
+        name: "root",
+        type: "list",
+        message: "||||   MAIN MENU   ||||",
+        choices: [
+            "VIEW ALL EMPLOYEES",
+            "VIEW ALL EMPLOYEES BY DEPARTMENT",
+            "VIEW ALL EMPLOYEES BY MANAGER",
+            "ADD AN EMPLOYEE",
+            "REMOVE AN EMPLOYEE",
+            "UPDATE EMPLOYEE ROLE",
+            "UPDATE EMPLOYEE MANAGER"
+        ]
+    });
+    conn.end();
+};
