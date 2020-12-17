c175b5b52b354515324725176c257d8b2040d35f
Seunghoon Lee <seunghoonlee@Seunghoons-MBP.nj.rr.com>
2020-12-17 16:21:03 -0500

    Remove an employee has been developed

--- ems.js
+++ ems.js
@@ -33,7 +33,6 @@ function main(res) {
         choices: [
             "VIEW ALL EMPLOYEES",
             "VIEW ALL EMPLOYEES BY DEPARTMENT",
-            "VIEW ALL EMPLOYEES BY MANAGER",
             "ADD AN EMPLOYEE",
             "REMOVE AN EMPLOYEE",
             "UPDATE EMPLOYEE ROLE",
@@ -104,16 +103,101 @@ function main(res) {
                 // main function
                 break;
             
-            case "VIEW ALL EMPLOYEES BY MANAGER":
-                function viewListByManager() {};
-                // main function
-                break;
             case "ADD AN EMPLOYEE":
-                function addPerson() {};
+                inquirer.prompt([{
+                    name: "first_name",
+                    type: "input",
+                    message: "First name? "
+                }, {
+                    name: "last_name",
+                    type: "input",
+                    message: "Last name? "
+                }, {
+                    name: "title",
+                    type: "list",
+                    message: "Title? ",
+                    choices: [
+                        "Project Manager",
+                        "Developer",
+                        "Accountant",
+                        "Finance Manager",
+                        "Marketer"
+                    ]
+                }]).then(function(data) {
+                    switch (data.title) {
+                        case "Project Manager":
+                            // SQL Addition
+                            conn.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                                        VALUES ("${data.first_name}", "${data.last_name}", 1, 3);`, function(err, res) {
+                                if (err) throw err;
+                                ems_init();
+                            })
+                            break;
+                        case "Developer":
+                            // SQL Addition
+                            conn.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                            VALUES ("${data.first_name}", "${data.last_name}", 2, 3);`, function(err, res) {
+                                if (err) throw err;
+                                ems_init();
+                            })
+                            break;                        
+                        case "Accountant":
+                            // SQL Addition
+                            conn.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                            VALUES ("${data.first_name}", "${data.last_name}", 3, 3);`, function(err, res) {
+                                if (err) throw err;
+                                ems_init();
+                            })
+                            break;
+                        case "Finance Manager":
+                            // SQL Addition
+                            conn.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                            VALUES ("${data.first_name}", "${data.last_name}", 4, 3);`, function(err, res) {
+                                if (err) throw err;
+                                ems_init();
+                            })
+                            break;
+                        case "Marketer":
+                            // SQL Addition
+                            conn.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                            VALUES ("${data.first_name}", "${data.last_name}", 5, 3);`, function(err, res) {
+                                if (err) throw err;
+                                ems_init();
+                            })
+                            break;
+                    }
+                });
                 // main function
                 break;
             case "REMOVE AN EMPLOYEE":
-                function removePerson() {};
+
+                conn.query("SELECT employee.id, first_name, last_name, title, salary, name FROM ((employee INNER JOIN roles ON employee.role_id = roles.id) INNER JOIN department ON employee.manager_id = department.id);", function(err, res) {
+                    if (err) throw err;
+                    printResult(res);
+
+                    inquirer.prompt({
+                        name: "removeId",
+                        type: "input",
+                        message: "Please enter the employee's ID: "
+                    }).then(function(ans) {
+                        console.log(ans.removeId);
+                        conn.query(`DELETE FROM employee WHERE id = ${ans.removeId}`, function(err) {
+                            if (err) throw err;
+                            ems_init();
+                        });
+                    });
+
+                });
+
+                // .then(function(answ) {
+                //     inquirer.prompt({
+                //         name: "main",
+                //         type: "input",
+                //         message: "Please enter the employee's ID: "
+                //     }).then(function(){
+                //         //
+                //     })
+                // })
                 // main function
                 break;
             case "UPDATE EMPLOYEE ROLE":
